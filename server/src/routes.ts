3ce93150239a4b89d72146f0abe481e54aef1637
victor-medeiros <victor.msilva@bandtec.com.br>
2020-08-06 01:39:42 -0300

    Listagem de uma aula

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,69 +1,14 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/ClassesController';
 
 const routes = express.Router();
+const classesController = new ClassesController();
 
 routes.get('/', (request, response) => {
     return response.json('Hello world!');
 });
 
-interface ScheduleItem {
-    week_day: number,
-    from: string,
-    to: string
-}
-
-routes.post('/classes', async (request, response) => {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = request.body;
-
-    const trx = await db.transaction();
-
-    try {
-        const insertedUsersIds = await trx('users').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio
-        });
-    
-        const user_id = insertedUsersIds[0];
-    
-        const insertedClassesIds = await trx('classes').insert({
-            user_id,
-            subject,
-            cost
-        });
-    
-        const class_id = insertedClassesIds[0];
-    
-        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-            return {
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: convertHourToMinutes(scheduleItem.from),
-                to: convertHourToMinutes(scheduleItem.to)
-            }
-        })
-    
-        await trx('class_schedule').insert(classSchedule);
-
-        await trx.commit();
-    
-        return response.send();
-    } catch (err) {
-        await trx.rollback();
-        
-        return response.status(400).json({ error: err });
-    }
-})
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
 
 export default routes;
